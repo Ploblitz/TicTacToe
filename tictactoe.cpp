7c38e5066cdc74e8ec4efed70d4a7fa861a92019
ThomasGallagher1 <118356796+ThomasGallagher1@users.noreply.github.com>
2023-03-09 14:40:34 +0000

    hhh
    wdaw

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,15 +7,11 @@
 int PlayerGo(int x);
 int PCGo(int y);
 int Game(int a);
-int Work(int b);
 
 int main()
 {
-	int game, a,b,x,y,i;
+	int game, a, b = 0, x, y, i;
 
-	printf("Please enter where you want to place your X");
-	scanf("%i", &i);
-	return 1;
 	game = Game(a);
 
 	if (game > 1)
@@ -31,20 +27,34 @@ int main()
 
 int PlayerGo(int x)
 {
+	for (int j = 0; j < 5; j++)
+	{
+		printf("Hi");
 
-
-
-	return 1;
+		return 1;
+	}
+	return 0;
 }
 
 int PCGo(int y)
 {
+	for (int j = 0; j < 5; j++)
+	{
+		printf("hello");
 
-
-
-	return 2; 
+		return 2;
+	}
+	return 0;
 }
 
+int Game(int a)
+{
+	int i;
+	printf("Please enter where you wantrrewetwertrtw to place your X");
+	scanf_s("%i", &i);
+
+	return 2;
+}
 //matrix for displaying Xs and Os
 
 //diagonal matrix to decide the winner only for diagonal
