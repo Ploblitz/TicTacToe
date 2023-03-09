c4df62104cb5db6267f8de2e6126112a3b71768b
ThomasGallagher1 <118356796+ThomasGallagher1@users.noreply.github.com>
2023-03-09 14:27:33 +0000

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,6 +1,56 @@
-// eddie, michael, thomas
-// Tictactoe project
-// 09/03/2023
+//Thomas Gallagher
+//Edmond Wilkinson
+//Micheal Morris
 
 #include <stdio.h>
 
+int PlayerGo(int x);
+int PCGo(int y);
+int Game(int a);
+int Work(int b);
+
+int main()
+{
+	int game, a,b,x,y,i;
+
+	printf("Please enter where you want to place your X");
+	scanf("%i", &i);
+	return 1;
+	game = Game(a);
+
+	if (game > 1)
+	{
+		return PlayerGo(x);
+	}
+	else
+	{
+		return PCGo(y);
+	}
+
+}
+
+int PlayerGo(int x)
+{
+
+
+
+	return 1;
+}
+
+int PCGo(int y)
+{
+
+
+
+	return 2; 
+}
+
+//matrix for displaying Xs and Os
+
+//diagonal matrix to decide the winner only for diagonal
+
+// return 0, return 1 to swap between go's
+
+// X and O for unique player
+
+// random number for the PC
